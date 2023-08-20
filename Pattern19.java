0e2c8917c4ac21953a3ec73fafc97107fc1c83cf
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-20 15:15:50 +0530

    Add files via upload

---
+++ Pattern19.java
@@ -0,0 +1,10 @@
+public class Pattern19 {
+    public static void main(String[] args) {
+        for(int i=70;i>=65;i--){
+            for(int j=i;j>=65;j--){
+                System.out.print((char)i);
+            }
+            System.out.println();
+        }
+    }
+}
